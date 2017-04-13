1c5b2ddf87f84a5ed1a2e8c86d0b3936763e2656
Jeremy Felt <jeremy.felt@gmail.com>
2017-04-13 11:59:07 -0700

    Catch pending orders that do not yet have CC entered

--- includes/class-wsuws-gateway-response.php
+++ includes/class-wsuws-gateway-response.php
@@ -30,13 +30,18 @@ class WSUWS_Gateway_Response {
 	public function check_response( $order_id ) {
 		$order = wc_get_order( $order_id );
 
-		// If a GUID is not set, no response can be checked.
 		if ( ! isset( $_GET['GUID'] ) ) { // @codingStandardsIgnoreLine
-			return;
+			// Payment has not been attempted via Cybersource. (back button?)
+			if ( 'pending' === $order->get_status() ) {
+				wc_add_notice( 'Payment has not been completed. Please try again.', 'error' );
+				return;
+			} else {
+				return;
+			}
 		}
 
 		// If an order is past the point of authorization, then we should not be running this again.
-		$skip_order_status = array( 'on-hold', 'completed', 'cancelled', 'refunded', 'failed' );
+		$skip_order_status = array( 'on-hold', 'processing', 'completed', 'cancelled', 'refunded', 'failed' );
 		if ( in_array( $order->get_status(), $skip_order_status, true ) ) {
 			return;
 		}
